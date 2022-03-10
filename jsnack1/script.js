e22b77369b069bc33cef2c739a2c7fa15db5fc38
luciomelis <luciomelis1992@gmail.com>
2022-03-10 16:00:13 +0100

    finish jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,19 +4,23 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 */
 
+
 const guestList = ["Ciro", "Luca", "Erick", "Stefano", "Lucio", "Giova"];
 const nameUser = prompt('Inserisci il nome');
 
-let controlGuest = false;
+let trovato = false;
 
-for (let i = 0; i < guestList.length; i++) {
+for (let i = 0; i < guestList.length && trovato === false; i++) {
+
     if (guestList[i] === nameUser.trim()) {
-        controlGuest = true;
+        trovato = true;
     }
 }
 
-if (controlGuest === true) {
+if (trovato === true) {
     console.log('Puoi partecipare alla festa');
 } else {
     console.log('Non puoi partecipare alla festa');
 }
+
+
