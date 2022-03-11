0d3c88fcf0a976baa7b50ae1f05dbc625fa10ab3
luciomelis <luciomelis1992@gmail.com>
2022-03-11 10:23:05 +0100

    changes

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -8,10 +8,29 @@ const containerNumberOdd = [];
 
 for (i = 0; i < 6; i++) {
     let userNumber = parseInt(prompt('Inserisci un numero'));
-    if (userNumber % 2 === 1) {
+    if (userNumber % 2 === 1) /*if (userNumber % 2 !== 0) meglio*/ {
         containerNumberOdd.push(userNumber);
     } else {
         console.log('hai inserito un numero pari');
     }
 }
 console.log(containerNumberOdd);
+
+
+// versione corretta e completa -->
+
+/*
+const arrayNumeri = [];
+
+for (let i = 0; i < 6; i++) {
+    let nuovoNumero;
+    while (isNaN(nuovoNumero)) {
+        nuovoNumero = parseInt(prompt(`${i +} Inserisci un nuovo numero`));
+    }
+    if (nuovoNumero % 2 !== 0) {
+        arrayNumeri.push(nuovoNumero);
+    }
+}
+
+console.log('I numeri dispari che hai inserito sono: ' + arrayNumeri);
+*/
