f84ef0a8ae502d1356ed690bbc5da3390bc43f3e
luciomelis <luciomelis1992@gmail.com>
2022-03-11 10:44:05 +0100

    correzzione

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -14,3 +14,17 @@ for (let i = 0; i < number.length; i++) {
 }
 
 console.log(sum);
+
+/*
+// correzzione esercizio 
+let numeroUtente;
+while (isNaN(parseInt(numeroUtente)) || numeroUtente.length !== 4) {
+    numeroUtente = prompt('Inserisci un numero di 4 cifre');
+}
+let sommaCifre = 0;
+for (let i = 0; i < numeroUtente.length; i++) {
+    const carattere = numeroUtente[i];
+    sommaCifre += parseInt(carattere);
+}
+console.log(`La somma delle cifre del numero ${numeroUtente} è: ${sommaCifre}`);
+*/
